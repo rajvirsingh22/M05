d93b53c0d7b146a4b28e8025eb84a854d3504f5f
rajvirsingh22 <rajvirsingh22@ilg.cat>
2022-10-23 18:48:17 +0200

    locale

--- Program.java
+++ Program.java
@@ -10,7 +10,7 @@
  * See http://www.gnu.org/licenses/gpl.html for more information.
  */
 package gitTerminal02;
-
+ndvjbzjkbibj
 import java.util.Scanner;
 
 public class Program {
@@ -35,7 +35,7 @@ public class Program {
 
 		return (2 * Math.PI) * radius;
 	}
-
+	njbkhkjbrajvir
 	/**
 	 * TUI = Terminal User Interface
 	 * 
