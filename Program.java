538828a2236b3a35adf6559740a23b9778066e75
rajvirsingh22 <rajvirsingh22@ilg.cat>
2022-10-23 18:52:42 +0200

    perimeter

--- Program.java
+++ Program.java
@@ -4,7 +4,7 @@
  * Program to calculate the area and the perimeter
  * of a circle knowing the radius in meter.
  *
- * Copyright 2020 solobro Sèculi <rajvirsingh22@ilg.cat>
+ * Copyright 2020 rajvirsingh Sèculi <rajvirsingh22@ilg.cat>
  *
  * This is free software, licensed under the GNU General Public License v3.
  * See http://www.gnu.org/licenses/gpl.html for more information.
@@ -56,3 +56,4 @@ public class Program {
 
 	}
 }
+
